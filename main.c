be3452a0291f307d0a2a03e4c260014f2324a351
Mitchell Clay <55418217+mitchellclay-asu@users.noreply.github.com>
2020-12-15 16:55:26 -0600

    added getopt() parameters

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 struct Sensor {
     double terminal_velocity;
@@ -51,7 +54,6 @@ int update_position(struct Sensor sensors[], int sensor_count, double time_resol
     return 0;
 }
 
-
 int clock_tick(struct Sensor sensors[], 
                int sensor_count, 
                double* current_time, 
@@ -63,8 +65,9 @@ int clock_tick(struct Sensor sensors[],
     return 0;
 }
 
-int main () {
-    // Initialization
+int main(int argc, char **argv) {
+    // Initialization and defaults
+    clock_t t1, t2;
     int sensor_count = 10;
     int moving_sensors = 0; 
     int ret = 0;
@@ -72,9 +75,46 @@ int main () {
     double start_x = 0;
     double start_y = 0;
     double start_z = 1000;
-    struct Sensor sensors[sensor_count];
     double current_time = 0;
     double time_resolution = 0.001;
+    int debug = 0;
+    int verbose = 1;
+
+    // get command line switches
+    int c;
+    while ((c = getopt(argc, argv, "d:v:c:g:r:z:")) != -1)
+    switch (c) {
+        case 'd':
+            debug = atoi(optarg);
+            break;
+        case 'v':
+            verbose = atoi(optarg);
+            break;
+        case 'c':
+            sensor_count = atoi(optarg);
+            break;
+        case 'g':
+            gravity = atof(optarg);
+            break;
+        case 'r':
+            time_resolution = atof(optarg);
+            break;
+        case 'z':
+            start_z = atof(optarg);
+            break;
+        case '?':
+            if (optopt == 'c')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr,
+                         "Unknown option character `\\x%x'.\n",
+                         optopt);
+                return 1;
+        default:
+            abort ();
+    }
     
     // Output parameters
     printf("Number of sensors: %d\n", sensor_count);
@@ -84,6 +124,8 @@ int main () {
 
     // Get sensors ready
     printf("Sensor initialization: ");
+    struct Sensor sensors[sensor_count];
+
     ret = initialize_sensors(sensors, sensor_count, start_x, start_y, start_z);
     if (ret == 0) {
         printf("OK\n");
@@ -92,6 +134,8 @@ int main () {
     
     // Run until all sensors reach z = 0;
     printf("Running simulation\n");
+    t1 = clock();
+
     while (moving_sensors != 0) {
         clock_tick(sensors, sensor_count, &current_time, time_resolution, gravity);
         moving_sensors = 0; 
@@ -100,14 +144,23 @@ int main () {
                 moving_sensors++;
             }
         }
-    }        
-
-    // Print final positions
-    for (int i = 0; i < sensor_count; i++) {
-        printf("Sensor %d final velocity: %f m/s, final position: (%f, %f, %f)\n", 
-               i, sensors[i].z_velocity, sensors[i].x_pos, sensors[i].y_pos, sensors[i].z_pos);
     }
-    printf("Final time: %f seconds\n", current_time);
+
+    // Calculate simulation time
+    t2 = clock();
+    double runTime = (double)(t2 - t1) / CLOCKS_PER_SEC;
+
+    // Print summary information
+    printf("Simulation complete\n");
+    printf("Simulation time: %f seconds\n", runTime);        
+
+    if (debug) {
+        for (int i = 0; i < sensor_count; i++) {
+            printf("Sensor %d final velocity: %f m/s, final position: (%f, %f, %f)\n", 
+                i, sensors[i].z_velocity, sensors[i].x_pos, sensors[i].y_pos, sensors[i].z_pos);
+        }
+    }
+    printf("Final clock time: %f seconds\n", current_time);
     
     return 0;
 }
