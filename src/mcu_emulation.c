10fcdbec0674a8ca36b6cfe5be31dfeea899a182
Mitchell Clay <mitchellclay@protonmail.com>
2020-12-31 11:51:26 -0600

    added documentation comments

--- src/mcu_emulation.c
+++ src/mcu_emulation.c
@@ -8,6 +8,10 @@
 
 #include "mcu_emulation.h"
 
+/**
+ * Microcontroller node selection
+ * Desc: Cycles through all nodes and calls MCU function handler for each
+**/
 int update_mcu(struct Node* nodes,
                int node_count,
                double time_resolution,
@@ -20,6 +24,19 @@ int update_mcu(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Microcontroller function selection handling
+ * Desc: See which function a nodes MCU should be executing and set/check busy times
+ *
+ * Functions:
+ * ----------------
+ * 0: main
+ * 1: scan_lfg
+ * 2: broadcast_lfg
+ * 3: find_clear_channel
+ * 4: check_channel_busy
+ *
+**/
 int mcu_run_function(struct Node* nodes,
                      int node_count,
                      int id,
@@ -78,6 +95,23 @@ int mcu_run_function(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Function Number:             0
+ * Function Name:               main
+ * Function Description:        Main Loop of microcontroller emulation routine
+ * Function Busy times:         None
+ * Function Return Labels:      2
+ 
+ * Return Label 0 returns from: 1 (scan_lfg)
+ * Return Lable 0 reason:       Check to see if scan function finds a looking for 
+                                group broadcast message
+ 
+ * Return Label 1 returns from: 2 (broadcast_lfg)
+ * Return Label 1 reason:       Check to see if LFG was successfully broadcast on
+                                an open channel
+
+ * Function Returns:            nothing
+**/
 int mcu_function_main(struct Node* nodes,
                      int node_count,
                      int id,
@@ -92,6 +126,16 @@ int mcu_function_main(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Function Number:             1
+ * Function Name:               scan_lfg
+ * Function Description:        Scan all available channel and look for a LFG broadcast
+ * Function Busy times:         50 ms per channel scanned
+ * Function Return Labels:      0
+ *
+ * Function Returns:            0 - no LFG found
+ *                              ID - node broadcasting LFG with <ID>
+**/
 int mcu_function_scan_lfg(struct Node* nodes,
                           int node_count,
                           int id,
@@ -113,6 +157,20 @@ int mcu_function_scan_lfg(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Function Number:             2
+ * Function Name:               broadcast_lfg
+ * Function Description:        Broadcasts LFG message 
+ * Function Busy times:         2.0 seconds for broadcast + 50 ms/channel scanning
+ * Function Return Labels:      1
+ 
+ * Return Label 0 returns from: 3 (find_clear_channel)
+ * Return Lable 0 reason:       Get return value of first available channel for
+                                broadcasting (if any)
+
+ * Function Returns:            0 - no clear channels
+ *                              channel - sent LFG on <channel>
+**/
 int mcu_function_broadcast_lfg(struct Node* nodes,
                                int id,
                                int group_max,
@@ -127,6 +185,20 @@ int mcu_function_broadcast_lfg(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Function Number:             3
+ * Function Name:               find_clear_channel
+ * Function Description:        Listens for any node transmitting and finds first
+                                available free channel
+ * Function Busy times:         50ms per channel scanned
+ * Function Return Labels:      1
+ 
+ * Return Label 0 returns from: 4 (check_channel_busy)
+ * Return Lable 0 reason:       See if there is any activity on selected channel
+
+ * Function Returns:            0 - no clear channels
+ *                              channel - first available free channel
+**/
 int mcu_function_find_clear_channel(struct Node* nodes,
                                     int node_count,
                                     int id,
@@ -150,6 +222,17 @@ int mcu_function_find_clear_channel(struct Node* nodes,
     return 0;
 }
 
+/**
+ * Function Number:             4
+ * Function Name:               check_channel_busy
+ * Function Description:        Checks to see if there are any transmissions on 
+                                selected channel
+ * Function Busy times:         50ms 
+ * Function Return Labels:      0
+
+ * Function Returns:            0 - channel free
+ *                              1 - channel busy
+**/
 int mcu_function_check_channel_busy(struct Node* nodes,
                                     int node_count,
                                     int id,
@@ -166,6 +249,7 @@ int mcu_function_check_channel_busy(struct Node* nodes,
     return 0;    
 }
 
+// update node busy times
 int mcu_update_busy_time(struct Node* nodes,
                          int id,
                          double time_resolution,
