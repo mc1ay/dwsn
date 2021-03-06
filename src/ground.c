c7da99722ae56d530782d5367412703d36a17f26
Mitchell Clay <mitchellclay@protonmail.com>
2021-03-06 12:54:19 -0600

    added message processing to ground station

--- src/ground.c
+++ src/ground.c
@@ -6,6 +6,7 @@
  * @date    3/6/2021
 **/
 
+#include <string.h>
 #include "ground.h"
 #include "settings.h"
 #include "state.h"
@@ -38,8 +39,42 @@ int update_ground(struct Node* nodes, struct Ground_Station* ground) {
         if (signals_detected > 1) {
             ground->collisions_detected++;
         }
-        else {
-            // process messages
+        else if (signals_detected == 1) {
+            // Check for DATA message
+            char* token;
+            char incoming_buffer[256];
+            char message[256];
+
+            // Zero out message string to eliminate proceeded garbage data
+            bzero(message, 256);
+
+            strncpy(incoming_buffer, nodes[transmitting_node].send_packet, 256);
+            token = strtok(incoming_buffer, " ");
+            char sender_id[6];
+            
+            // Extract dest and src node IDs from message
+            if (strcmp(token, "GROUND") == 0) {
+                token = strtok(NULL, " ");
+                strncpy(sender_id, token, 6);
+
+                // Check if third token is "DATA"
+                token = strtok(NULL, " ");
+                if (strcmp(token, "RELAY") == 0) {
+                    // Process remaining tokens
+                    token = strtok(NULL, " ");
+                    do {
+                        strncat(message, token, strlen(token));
+                        strncat(message, " ", 2);
+                        token = strtok(NULL, " ");
+                    } while (token != 0);
+
+                    // Update message counter and write to file if output flag set
+                    ground->messages_received++;
+                    if (settings.output) {
+                        // write to log file
+                    }
+                }
+            }
         }
     }
     return 0;
